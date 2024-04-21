a4f0180de46a847b5a0b5e794341f4fad3ea298b
Asgeir Melling <asgmel@gmail.com>
2024-04-21 10:24:22 +0200

    feat: coded task one of day one

--- src/aoc/days/day_01/main.py
+++ src/aoc/days/day_01/main.py
@@ -1,9 +1,17 @@
 from pathlib import Path
 from typing import Optional
 
-from aoc.utils.parse_input import read_input_as_list_of_strings
+from aoc.utils.parse_input import get_input_to_use, read_input_as_list_of_strings
 
 
-def run(input: Optional[Path]):
-    puzzle_input = read_input_as_list_of_strings(input=input)
-    print(puzzle_input[0])
+def run(input: Optional[Path]) -> None:
+    input_to_use = get_input_to_use(day=1, input=input)
+    puzzle_input = read_input_as_list_of_strings(input=input_to_use)
+
+    task_one(input=puzzle_input)
+
+
+def task_one(input: list[str]) -> None:
+    numbers = map(lambda row: list(filter(lambda y: y.isdigit(), row)), input)
+
+    print(f"Puzzle answer is: { sum([row[0] + row[-1] for row in numbers]) }")
--- src/aoc/utils/parse_input.py
+++ src/aoc/utils/parse_input.py
@@ -1,9 +1,27 @@
+import inspect
 from pathlib import Path
 from typing import Optional
 
+import aoc
 
-def read_input_as_string(input: Optional[Path]) -> str:
-    with open(input if input else "input.txt", "r", encoding="utf-8") as file:
+
+def get_input_to_use(input: Optional[Path], day: int) -> Path:
+    return input if input else get_input_path_from_day(day=day)
+
+
+def get_input_path_from_day(day: int) -> Path:
+    if day < 1 or day > 24:
+        raise ValueError(
+            f"Parameter 'day' to 'get_input_path_from_day' must be between 1 and 24. Value '{day}' not supported."
+        )
+
+    root_module_file_path = Path(inspect.getfile(aoc)).parent
+    folder_name = f"day_0{day}" if day < 10 else f"day_{day}"
+    return root_module_file_path / f"days/{folder_name}/input.txt"
+
+
+def read_input_as_string(input: Path) -> str:
+    with open(input, "r", encoding="utf-8") as file:
         file_contents = file.read()
 
     return file_contents
